0e8432f19ec038cf3b780ac1a51eed44f8f42438
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-08-01 11:11:36 +0200

    Update styling

--- app/page.js
+++ app/page.js
@@ -49,7 +49,7 @@ export default async function Home() {
 
 {/* <TestimonialsCarousel/> */}
 <NewsSection/>
-<HorsesIntroSection/>
+{/* <HorsesIntroSection/> */}
 
 <AboutFirst/>
 
--- components/main-page/offer-section/index.js
+++ components/main-page/offer-section/index.js
@@ -1,12 +1,17 @@
+import ButtonLg from '@/components/global-components/buttton-lg';
 import React from 'react'
 
 const OfferSection = () => {
   return (
     <section className="grid grid-cols-2 w-screen h-screen">
        <div className="relative  bg-[url('/offer-img.svg')] h-screen w-screen bg-contain bg-center bg-no-repeat mx-auto">
-<div className="w-full h-full grid grid-cols-2">
-<button>business offer</button>
-<button>view my exercises</button>
+<div className="relative w-full h-full">
+  <div className="inline-block absolute top-[55%] left-[32%]">
+<ButtonLg isOrange>horses for sale</ButtonLg>
+</div>
+<div className="inline-block absolute top-[55%] right-[32%]">
+<ButtonLg>view my exercises</ButtonLg>
+</div>
 </div>
 </div>
 
