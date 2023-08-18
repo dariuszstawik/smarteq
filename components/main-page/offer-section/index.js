ce76b1312faf7801e01596ab7b0e9f011f3eaafc
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-08-18 11:58:03 +0200

    Update styling

--- components/main-page/offer-section/index.js
+++ components/main-page/offer-section/index.js
@@ -5,11 +5,11 @@ import React from "react";
 const OfferSection = () => {
   return (
     <section className="container mx-auto h-[calc(100vh-112px)] mt-28">
-      <div className="bg-[url('/offer-img.svg')] w-full h-full bg-contain bg-center bg-no-repeat mx-auto ">
-        <div className="w-full h-full grid grid-cols-1 lg:grid-cols-2 gap-6 place-content-center">
-          <div className="relative flex flex-col w-full items-end">
-            <div classname="flex flex-col justify-end">
-              <div className="h-[292px] w-1/2">
+      <div className="lg:bg-[url('/offer-img.svg')] w-full h-full bg-contain bg-center bg-no-repeat mx-auto ">
+        <div className="w-full h-full grid grid-cols-1 lg:grid-cols-2 gap-6 place-content-center mx-auto">
+          <div className="relative flex flex-col-reverse lg:flex-col w-full items-center justify-center">
+            <div classname="flex flex-col justify-center items-center">
+              <div className="hidden lg:block h-[292px] w-1/2">
                 <img
                   src="asset-dots.svg"
                   alt="asset-dots"
@@ -18,7 +18,7 @@ const OfferSection = () => {
                   className=""
                 />
               </div>
-              <ButtonLg isOrange className="mr-28">
+              <ButtonLg isOrange className="lg:mr-28">
                 horses for sale
               </ButtonLg>
             </div>
@@ -27,47 +27,34 @@ const OfferSection = () => {
               alt="woman graphic"
               width="113px"
               height="196px"
-              className="self-start ml-32"
+              className="lg:self-start mx-auto lg:ml-32"
             />
           </div>
 
-          <div className="relative flex flex-col w-full ">
-            <div classname="flex flex-col justify-end ">
-              <div className="h-[292px] w-1/2 flex flex-col place-content-end">
+          <div className="relative flex flex-col w-full">
+            <div classname="flex flex-col justify-center items-center">
+              <div className="mx-auto lg:mx-0 h-[292px] w-1/2 flex flex-col place-content-end items-center">
                 <img
                   src="horse-jumping.svg"
                   alt="horse jumping"
                   width="246px"
                   height="204px"
-                  className="ml-8 mb-8"
+                  className="lg:ml-8 mb-8"
                 />
               </div>
 
-              <ButtonLg className=" mr-auto">view my exercises</ButtonLg>
+              <ButtonLg className="mx-auto lg:mr-auto">
+                view my exercises
+              </ButtonLg>
             </div>
             <img
               src="asset-circle1.svg"
               alt="asset-circle"
               width="155px"
               height="137px"
-              className="absolute top-[26%] right-20"
+              className="hidden lg:block absolute top-[26%] right-20"
             />
           </div>
-
-          {/* <div className="basis-1/2 flex flex-col shrink-0">
-            <img src="asset-dots.svg" alt="asset-dots" />
-            <ButtonLg isOrange className="mx-auto">
-              horses for sale
-            </ButtonLg>
-          </div>
-          <div className="basis-1/2 shrink-0">
-            <img src="woman-img.svg" alt="woman grphic" className="w-36" />
-          </div>
-        </div>
-        <div className="inline-block ">
-          <ButtonLg>view my exercises</ButtonLg>
-        </div> */}
-          {/* </div> */}
         </div>
       </div>
     </section>
