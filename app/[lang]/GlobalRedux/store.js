345410a63d05de52c87879a9c83bfc37c5780143
KamilHrebeniuk <kamil.hrebeniuk@gmail.com>
2023-08-31 13:36:56 +0200

    Add to cart fix

--- app/[lang]/GlobalRedux/store.js
+++ app/[lang]/GlobalRedux/store.js
@@ -1,33 +1,6 @@
 "use client";
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const useCartEffect = () => {
-  useEffect(() => {
-    const cart = JSON.parse(localStorage.getItem("cart"));
-  }, []);
-  console.log("cart 123" + cart);
-  return cart[0];
-};
-
-const initialProductsState = [
-  {
-    productsList: [useCartEffect ? useCartEffect : ""],
-    selectedProduct: "",
-  },
-];
-
-const productsSlice = createSlice({
-  name: "products",
-  initialState: initialProductsState,
-  reducers: {
-    selectProduct(state, action) {
-      state.selectedProduct = action.payload;
-    },
-  },
-});
-
-export const { selectProduct } = productsSlice.actions;
-
 const initialCartState = [];
 
 const cartSlice = createSlice({
@@ -50,32 +23,10 @@ const cartSlice = createSlice({
 
 export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
 
-const initialIsModalOpenState = false;
-
-const modalSlice = createSlice({
-  name: "modal",
-  initialState: initialIsModalOpenState,
-  reducers: {
-    openModal(state) {
-      state = true;
-    },
-    closeModal(state) {
-      state = false;
-    },
-
-    toggleModal(state) {
-      state = !state;
-    },
-  },
-});
-
-export const { openModal, closeModal } = modalSlice.actions;
 
 export const store = configureStore({
   reducer: {
     cart: cartSlice.reducer,
-    products: productsSlice.reducer,
-    modal: modalSlice.reducer,
   },
 });
 
