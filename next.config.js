5fea8115865aec181c903a1748ab6708214f73a9
KamilHrebeniuk <kamil.hrebeniuk@gmail.com>
2023-08-31 13:48:04 +0200

    Add simple redirection

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ["localhost:3000", "images.ctfassets.net"],
   },
+    async rewrites() {
+      return [
+        {
+          source: '/',
+          destination: '/pl',
+        },
+      ]
+    },
 };
 
 module.exports = nextConfig;
