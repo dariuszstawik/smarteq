e53256b0f5e6835be4d9bc50066482db68daad69
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-10-10 14:31:26 +0200

    Update next.js config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   images: {
     domains: ["localhost:3000", "images.ctfassets.net"],
   },
-    async rewrites() {
-      return [
-        {
-          source: '/',
-          destination: '/pl',
-        },
-      ]
-    },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/pl",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
