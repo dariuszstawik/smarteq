4ffc658db50bfdfeacb633298b051bdd616816f5
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-10-11 13:54:13 +0200

    Fix errors in succes and cancel pages url

--- app/[lang]/api/checkout/route.js
+++ app/[lang]/api/checkout/route.js
@@ -19,8 +19,8 @@ export async function POST(request) {
     });
 
     const session = await stripe.checkout.sessions.create({
-      success_url: "https://smart-equestrian/pl/success",
-      cancel_url: "https://smart-equestrian/pl/cancel",
+      success_url: "https://smart-equestrian.pl/pl/success",
+      cancel_url: "https://smart-equestrian.pl/pl/cancel",
       line_items: body.lineItems,
       mode: "payment",
       shipping_address_collection: {
